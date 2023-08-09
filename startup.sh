b7dea1dcf6cce783654cadf5074ea640e9f87853
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-09 11:47:39 +0700

    finish

--- startup.sh
+++ startup.sh
@@ -1,14 +1,8 @@
-version: 1
-runtime: php81
-build:
-  commands:
-    build:
-      # shellcheck disable=SC2215
-      - chmod +x startup.sh
-run:
-  command: ./startup.sh
-  ports:
-    # shellcheck disable=SC2215
-    - 80
-  env:
-    APP_PORT: 80
+#!/bin/bash
+set -o monitor
+trap exit SIGINT
+# Start nginx
+nginx -g 'daemon off;' &
+# start php-fpm
+php-fpm -F &
+wait
