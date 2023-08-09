0a243c3950bb47bf918c79114fc2090ce0698f50
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-09 14:15:53 +0700

    bikin kode algoritma

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (isset($_GET['angka'])) {
     }
     function kedua($value)
     {
-        if($value == 0){
+        if ($value == 0) {
             return 0;
         }
         return ($value + 1) * $value / 2;
@@ -24,28 +24,41 @@ if (isset($_GET['angka'])) {
 
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pelatihan 01</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menghitung f(N)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
+<div class="contaiener mt-5">
+    <form method="get">
+        <div class="row">
+            <div class="col-6 mb-3">
+                <input type="number" class="form-control" name="angka" id="angka" min="0">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
-<form method="get">
-    <input type="number" name="angka" id="angka">
-    <button type="submit">Submit</button>
-</form>
-
-<?php if (isset($_GET['angka'])) : ?>
-    <h1>Fungsi pertama <?= pertama($angka); ?></h1>
-    <h1>Fungsi kedua <?= kedua($angka); ?></h1>
-    <h1>Fungsi Pertama Big O(<?= $angka != 0 ? $angka : 1; ?>)</h1>
-    <h1>Fungsi Kedua Big O(<?= 1 ?>)</h1>
-<?php endif; ?>
+    <?php if (isset($_GET['angka'])) : ?>
+        <div class="row">
+            <div class="col-6">
+                <h1>Fungsi pertama <?= pertama($angka); ?></h1>
+                <h1>Fungsi kedua <?= kedua($angka); ?></h1>
+            </div>
+            <div class="col-6">
+                <h1>Fungsi Pertama Big O(<?= $angka != 0 ? $angka : 1; ?>)</h1>
+                <h1>Fungsi Kedua Big O(<?= 1 ?>)</h1>
+            </div>
+        </div>
+    <?php endif; ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
