3fc454d65db8adce954d0d8ad1da086213973a89
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-09 14:00:40 +0700

    bikin kode algoritma

--- public/index.php
+++ public/index.php
@@ -1,31 +1,51 @@
 <?php
+if (isset($_GET['angka'])) {
+    $angka = $_GET['angka'];
 
-
-function kedua($value){
-    return ($value + 1) * $value/2;
-}
-function pertama($value){
-    $sum = 0;
-    for ($i=1; $i <= $value; $i++) {
-        $sum += $i;
+    function pertama($value)
+    {
+        if ($value == 0) {
+            return 0;
+        }
+        $sum = 0;
+        for ($i = 1; $i <= $value; $i++) {
+            $sum += $i;
+        }
+        return $sum;
     }
-    return $sum;
+    function kedua($value)
+    {
+        if($value == 0){
+            return 0;
+        }
+        return ($value + 1) * $value / 2;
+    }
 }
+
 
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pelatihan 01</title>
 </head>
+
 <body>
-    <h1>Fungsi pertama <?=  pertama(6); ?></h1>
-    <h1>Fungsi optimasi <?=  kedua(6); ?></h1>
+
+<form method="get">
+    <input type="number" name="angka" id="angka">
+    <button type="submit">Submit</button>
+</form>
+
+<?php if (isset($_GET['angka'])) : ?>
+    <h1>Fungsi pertama <?= pertama($angka); ?></h1>
+    <h1>Fungsi kedua <?= kedua($angka); ?></h1>
+    <h1>Fungsi Pertama Big O(<?= $angka != 0 ? $angka : 1; ?>)</h1>
+    <h1>Fungsi Kedua Big O(<?= 1 ?>)</h1>
+<?php endif; ?>
 </body>
+
 </html>
